e27d8a38b5bf32d28759a4c5d008ab6714c4e5f1
施凯伦 <kshi@linkedin.com>
2016-09-20 14:51:25 +0800

    update code

--- result.go
+++ result.go
@@ -5,3 +5,9 @@ type PushResult struct {
 	message    string `json:"message"`
 	requestID  string `json:"requestID"`
 }
+
+type NotificationResult struct {
+	resultCode string `json:"result_code"` // 0：成功
+	requestID  string `json:"request_id"`  // 由服务器生成，方便用户问题追查与定位
+	resultDesc string `json:"result_desc"` // 失败原因
+}
